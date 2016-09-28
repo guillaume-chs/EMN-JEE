c9ce6102c93e7a10f95e9899a22e9eff747320c7
anaelChardan <anael.chardan@gmail.com>
2016-09-28 15:17:54 +0200

    Initial Commit

---
+++ src/main/java/td/servlet/Hello.java
@@ -0,0 +1,42 @@
+package td.servlet ;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Hello extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        process(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        process(request, response);
+    }
+
+    protected void process(HttpServletRequest request,
+
+                           HttpServletResponse response)
+
+            throws ServletException, IOException
+
+    {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>Hello world ! </h1>");
+
+        out.println("<h1>getServletName() : " +
+
+                getServletName() + "</h1>");
+
+        out.println("<h1>URI : " +
+
+                request.getRequestURI() + "</h1>");
+
+    }
+}
