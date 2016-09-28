6e02588ba2d7ef6060ae0ee296eb5aa5dab60b56
anaelChardan <anael.chardan@gmail.com>
2016-09-28 16:50:01 +0200

    TD4

---
+++ src/main/java/td/servlet/Redirect.java
@@ -0,0 +1,46 @@
+package td.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author clementgarbay
+ */
+public class Redirect extends HttpServlet {
+
+    private String defaultPage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        defaultPage = config.getInitParameter("page");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        String contextPath = req.getContextPath();
+        String page = req.getParameter("page");
+        page = (page == null) ? defaultPage : page;
+
+        switch (page) {
+            case "1":
+                resp.sendRedirect(contextPath + "/index1.html");
+                break;
+            case "2":
+                resp.sendRedirect(contextPath + "/index2.html");
+                break;
+            case "g":
+                resp.sendRedirect("https://google.com");
+                break;
+            default:
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+}
